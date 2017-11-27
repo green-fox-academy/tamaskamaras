c2a17ecb9d4f671777fb0321482900d34e1089c3
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-27 17:41:52 +0100

    task 20

--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,2 +1,11 @@
-# Write a program that reads a number form the standard input,
-# Than prints "Odd" if the number is odd, or "Even" it it is even.
+# Write a program that reads a number from the standard input,
+# Than prints "Odd" if the number is odd, or "Even" if it is even.
+
+number = int(input())
+
+number %= 2
+
+if number == 0:
+    print("Even")
+elif number > 0:
+    print("Odd")
