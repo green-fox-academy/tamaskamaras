b6aa9887c24fde7d01c0edfea6f3005755076137
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-27 23:52:16 +0100

    task 22

--- week-02/day-01/party_indicator.py
+++ week-02/day-01/party_indicator.py
@@ -1,5 +1,5 @@
 # Write a program that asks for two numbers
-# Thw first number represents the number of girls that comes to a party, the
+# The first number represents the number of girls that comes to a party, the
 # second the boys
 # It should print: The party is exellent!
 # If the the number of girls and boys are equal and there are more people coming than 20
@@ -12,3 +12,17 @@
 #
 # It should print: Sausage party
 # If no girls are coming, regardless the count of the people
+
+girls = int(input("Provide the number of girls: "))
+boys = int(input("Provide the number of boys: "))
+
+sum = girls + boys
+
+if girls == boys and sum > 20:
+    print("The party is exellent!")
+elif not girls == boys and sum > 20:
+    print("Quite cool party")
+elif girls == 0:
+    print("Sausage party")
+elif sum < 20:
+    print("Average party...")
