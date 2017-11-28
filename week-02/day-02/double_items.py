8556212408d0096cbf589b5c62c0c96f2e673d04
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-28 17:32:22 +0100

    Double the items

---
+++ week-02/day-02/double_items.py
@@ -0,0 +1,10 @@
+# - Create an array variable named `ag`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Double all the values in the array
+
+ag = [3, 4, 5, 6, 7]
+
+for v in range(5):
+    ag[v] *= 2
+print(ag)
+
