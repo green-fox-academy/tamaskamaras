4cec1c773268b9d3351362a18ef959b191a82c95
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-29 15:54:32 +0100

    Match making

---
+++ week-02/day-03/L02matchmaking.py
@@ -0,0 +1,20 @@
+# Join the two lists by matching one girl with one boy in the order list
+# Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+girls = ["Eve", "Ashley", "Bözsi", "Kat", "Jane"]
+boys = ["Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"]
+order = []
+
+def matchmaking(g, b):
+    order = g
+    x = 0
+    for i in b:
+        order.insert(x, i)
+        x += 2
+    return(order)
+
+    # for b in boys:
+    #     order = order + [b]
+
+
+print(matchmaking(girls, boys))
