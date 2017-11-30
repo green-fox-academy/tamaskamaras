beb26bf8e50eefd97ff033cb2bf78a6c543d4689
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-30 15:22:10 +0100

    Function factorial

--- week-02/day-02/factorial.py
+++ week-02/day-02/factorial.py
@@ -1,10 +1,37 @@
 # - Create a function called `factorio`
 #   that returns it's input's factorial
 
+# a = int(input("Give me a number: "))
+# fact = 0
 
-def factorio():
-    fact = int(input("Give me a number: "))
-    for i in range(1, fact):
-        return(i)
+# def factorio(a):
+#     for i in range(1, a):
+#         fact += i
+#         print(fact)
+#     return i
 
-factorio()
+# factorio(a)
+# print(factorio(a))
+
+
+##############################################
+
+# a = int(input("Give me a number: "))
+# fact = a
+
+# for i in range(1, a):
+#     fact *= i
+# print(fact)
+
+##############################################
+
+a = int(input("Give me a number: "))
+
+def factorio(a):
+    fact = a
+    for i in range(1, a):
+        fact *= i
+    return fact
+
+factorio(a)
+print(factorio(a))
