636beeeb1fad857397b1b40e3b6f0a3cbc1c5c7e
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-11-30 17:23:39 +0100

    Student filter

---
+++ week-02/day-03/D02student_filter.py
@@ -0,0 +1,23 @@
+
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+
+# create a function that takes a list of students and prints:
+# - Who has got more candies than 4 candies
+
+# create a function that takes a list of students and prints: 
+#  - how many candies they have on average
+
+def four_candies(students):
+    names = ""
+    for stud in students:
+        if stud['candies'] > 4:
+            names += stud['name']
+    return names
+
+four_candies(students)
+print(four_candies(students))
