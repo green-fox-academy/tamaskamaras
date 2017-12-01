ecc3733c22fe8ceec37b1707f4c6275642537453
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-01 11:02:18 +0100

    Hoorray, 7

---
+++ week-02/day-03/L05element_finder.py
@@ -0,0 +1,11 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1, 2, 3, 4, 5, 6, 8]
+
+for num in numbers:
+    if num == 7:
+        result = "Hoorray"
+    else:
+        result = "Noooooo"
+
+print(result)
