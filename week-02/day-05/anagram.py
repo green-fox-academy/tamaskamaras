c2d10fbac8eb3af3e9c01fa3c9f68f73d2d95a65
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-03 11:55:17 +0100

    Before input

--- week-02/day-05/anagram.py
+++ week-02/day-05/anagram.py
@@ -1,38 +1,11 @@
-# def anagram(word1, word2):
-#     if len(word1) != len(word2):
-#         output = False
-#         if output != False:
-#             a = []
-#             b = []
-#             for i in range(len(word1)):
-#                 a.append(word1[i])
-#                 b.append(word2[i])
-#             return a, b
-#             print(a, b)
-# word1 = str(input("Type a word: "))
-# word2 = str(input("Type an anagram: "))
-# print(anagram(word1, word2))
 
+def anagram (a, b):
+    text1 = sorted(a)
+    text2 = sorted(b)
+    if text1 == text2:
+        output = True
+    else:
+        output = False
+    return output
 
-
-word1 = str(input("Type a word: "))
-word2 = str(input("Type an anagram: "))
-if len(word1) == len(word2):
-    quantity = True
-if quantity == True:
-    a = []
-    b = []
-    temp = []
-    for i in range(len(word1)):
-        a.append(word1[i])
-        b.append(word2[i])
-    for x in a:
-        for y in b:
-            if x == y:
-                temp.append(True)
-                if len(temp) == len(a):
-                    output = True
-                else:
-                    output = False
-
-print(output)
+print(anagram("asdf", "fdsa"))
