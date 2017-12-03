a9a5b7f3fb54c39ab5c96fa90470f309a86e6d95
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-03 15:16:46 +0100

    Works with lives

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -1,13 +1,15 @@
 from random import *
 
+lives = 10
 rang = int(input("Type the max range: "))
 x = randint(1, rang)
-guess = int(input("Type a guess: "))
-if guess > x:
-    print("Too high")
-elif guess < x:
-    print("Too low")
-else:
-    print("You won")
+for i in range(1, lives):
+    guess = int(input("Type a guess: "))
+    if guess > x:
+        print("Too high")
+    elif guess < x:
+        print("Too low")
+    else:
+        print("You won")
 
 print(x)
