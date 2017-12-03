1631c998a6c9466747a75267fc8660ec51f1ca44
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-03 15:10:44 +0100

    Conditions have been set

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -2,4 +2,12 @@ from random import *
 
 rang = int(input("Type the max range: "))
 x = randint(1, rang)
+guess = int(input("Type a guess: "))
+if guess > x:
+    print("Too high")
+elif guess < x:
+    print("Too low")
+else:
+    print("You won")
+
 print(x)
