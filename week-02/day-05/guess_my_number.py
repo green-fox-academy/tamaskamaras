5ce64824a86c38c93c177f52a9d9eda28801e34c
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-03 16:46:52 +0100

    Perfect operation

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -1,15 +1,18 @@
 from random import *
 
-lives = 10
-rang = int(input("Type the max range: "))
-x = randint(1, rang)
-for i in range(1, (lives + 1)):
-    guess = int(input("Type a guess: "))
-    if guess > x:
-        print("Too high. You have", (lives - i), "lives left.")
-    elif guess < x:
-        print("Too low. You have", (lives - i), "lives left.")
-    else:
-        print("You won!")
+def guess_my_number(rang):
+    lives = 10
+    x = randint(1, rang)
+    for i in range(1, (lives + 1)):
+        guess = int(input("Type a guess: "))
+        if guess > x:
+            print("Too high. You have", (lives - i), "lives left.")
+        elif guess < x:
+            print("Too low. You have", (lives - i), "lives left.")
+        else:
+            print("You won!")
+            return guess
 
-print(x)
+    print(x)
+rang = int(input("Type the max range: "))
+guess_my_number(rang)
