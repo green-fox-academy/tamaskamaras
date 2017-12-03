8907038c21a69e1907513e4edb04546ab2ad54eb
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-03 16:39:35 +0100

    Refining lives

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -3,13 +3,13 @@ from random import *
 lives = 10
 rang = int(input("Type the max range: "))
 x = randint(1, rang)
-for i in range(1, lives):
+for i in range(1, (lives + 1)):
     guess = int(input("Type a guess: "))
     if guess > x:
-        print("Too high")
+        print("Too high. You have", (lives - i), "lives left.")
     elif guess < x:
-        print("Too low")
+        print("Too low. You have", (lives - i), "lives left.")
     else:
-        print("You won")
+        print("You won!")
 
 print(x)
