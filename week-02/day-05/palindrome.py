5a9a9b740e9b98c65bd53011a0f94a431720f43a
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-04 21:36:11 +0100

    Finish palindrome

--- week-02/day-05/palindrome.py
+++ week-02/day-05/palindrome.py
@@ -2,13 +2,20 @@ text = "dog goat dad duck doodle never"
 
 def search_palindrome(text):
     palindrome = ""
-    for i in range(len(text) - 2):
+    for i in range(len(text) - 4):
         if text[i] == text[i + 2]:
             palindrome += text[i]
             palindrome += text[i + 1]
             palindrome += text[i + 2]
             palindrome += ", "
+        if text[i] == text[i + 4] and text[i + 1] == text[i + 3]:
+            palindrome += text[i]
+            palindrome += text[i + 1]
+            palindrome += text[i + 2]
+            palindrome += text[i + 3]
+            palindrome += text[i + 4]
+            palindrome += ", "
     return palindrome
 
     print(palindrome)
-print(search_palindrome("dog goat dad duck doodle never"))
+print(search_palindrome(text))
