4e8e6b10c2f4b7dc72f929be5241bfae6ee55903
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-05 16:27:56 +0100

    Works: cut inequal letters

--- week-03/day-02/07doubled.py
+++ week-03/day-02/07doubled.py
@@ -1,20 +1,36 @@
 # Create a method that decrypts the duplicated-chars.txt
 
-# def decrypt(file_name):
-#     duplicated-chars = open("duplicated-chars.txt", "r")
-#     lines = duplicated-chars.readlines()
-#     for line in lines:
-#         for i in range(len(line)
+def decrypt(file_name):
+    duplicated = open(file_name, "r")
+    dupli = duplicated.readlines()
+    for line in dupli:
+        newline = ""
+        for i in range(len(line) - 1):
+            if line[i] != line[i + 1]:
+                newline += line[i]
+        newline += "\n"
+        simplified_chars = open("simplified_chars.txt", "a")
+        simplified_chars.write(newline)
+    duplicated.close()
+    simplified_chars.close()
+
+decrypt("duplicated-chars.txt")
+
+###########################
 
 
-############################
+# line = "TThhee  ZZeenn  ooff  PPyytthhoonn"
+# new = ""
+
+# for i in range(len(line) - 1):
+#     if line[i] != line[i + 1]:
+#         new += line[i]
+
+# print(new)
 
 
-line = "TThhee  ZZeenn  ooff  PPyytthhoonn"
-new = ""
+# ##########################
 
-for i in range(len(line) - 1):
-    if line[i] != line[i + 1]:
-        new += line[i]
-
-print(new)
+# duplicatedchars = open("duplicated-chars.txt", "r")
+# lines = duplicatedchars.readlines()
+# print(lines)
