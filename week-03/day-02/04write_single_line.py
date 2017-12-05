9e939801d4100b4f4dadf972a687f0ac28d4bb1e
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-05 14:10:49 +0100

    Multiple lines

---
+++ week-03/day-02/04write_single_line.py
@@ -0,0 +1,10 @@
+# Open a file called "my-file1.txt"
+# Write your name in it as a single line
+# If the program is unable to write the file,
+# then it should print an error message like: "Unable to write file: my-file.txt"
+
+try:
+    openfile = open("my-file1.txt", "r")
+    openfile.write("Tamas Kamaras")
+except:
+    print("Unable to write file: my-file1.txt")
