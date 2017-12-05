b20b1f2ceae4d864352e5ef2314c51e48e5460d0
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-05 14:56:53 +0100

    Copy file

---
+++ week-03/day-02/06copy_file.py
@@ -0,0 +1,16 @@
+# Write a function that copies a file to an other
+# It should take the filenames as parameters
+# It should return a boolean that shows if the copy was successful
+
+def copy_file(filename1, filename2):
+    copy = open(filename1, "r")
+    file2 = open(filename2, "w")
+    file2.write(copy.read())
+    copy.close()
+    file2.close()
+    return True
+
+filename1 = "C:/Users/K/Desktop/greenfox/tamaskamaras/week-03/day-02/my-file.txt"
+filename2 = "C:/Users/K/Desktop/greenfox/tamaskamaras/week-03/day-02/my-file1.txt"
+
+print(copy_file(filename1, filename2))
