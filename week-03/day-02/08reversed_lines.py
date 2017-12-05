b062727898f6634446f81b70ef60002053b6c594
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-05 17:32:19 +0100

    Before finalizing 08 task

---
+++ week-03/day-02/08reversed_lines.py
@@ -0,0 +1,14 @@
+# Create a method that decrypts reversed-lines.txt
+
+def decrypt(file_name):
+    reverse = open(file_name, "r")
+    rev = reverse.readlines()
+    for line in rev:
+        newline = ""
+        for i in range(len(line) - 1, -1, -1):
+            newline += line[i]
+        newline += "\n"
+        simple = open("simple_lines.txt", "a")
+        simple.write(newline)
+
+decrypt("reversed-lines.txt")
