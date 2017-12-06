da801e85c13eedcf63a4059c2847f846ae294acc
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-06 17:58:24 +0100

    Task 14 starts to work

---
+++ week-03/day-03/14function_to_center.py
@@ -0,0 +1,27 @@
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a line from that point to the center of the canvas.
+# fill the canvas with lines from the edges, every 20 px, to the center.
+
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+def center_line ():
+    for a in range(0, 300, 20):
+        x = a
+        y = 0
+        line = canvas.create_line(
+            x,
+            y,
+            150,
+            150
+        )
+
+
+center_line()
+
+root.mainloop()
