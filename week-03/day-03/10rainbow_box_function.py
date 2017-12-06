fc5a2c855e859d413e22c7c95eda35a34830cc38
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-06 13:18:28 +0100

    Rainbow done

--- week-03/day-03/10rainbow_box_function.py
+++ week-03/day-03/10rainbow_box_function.py
@@ -17,15 +17,6 @@ def square_drawing (size, color):
     for i in range(6):
         box = my_canvas.create_rectangle(0 + (i * unit), 0 + (i * unit), 300 - (i * unit), 300 - (i * unit), fill = rainbow[i])
 
-square_drawing(5, 'black')
+    centerbox = my_canvas.create_rectangle(150 - (size / 2), 150 - (size / 2), 150 + (size / 2), 150 + (size / 2), fill = color)
+square_drawing(200, 'black')
 my_root.mainloop()
-
-
-
-    # box 300 distance - 0
-    # box 260 distance - 1 size
-    # box 220 distance - 2 size
-    # box 180 distance - 3 size
-    # box 140 distance - 4 size
-    # box 100 distance - 5 size
-    # box 60  size
