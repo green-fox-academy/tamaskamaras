df7fd54203d8fcb4c9a0a1033eb25ee93a578c59
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-06 13:02:12 +0100

    Almost done

--- week-03/day-03/10rainbow_box_function.py
+++ week-03/day-03/10rainbow_box_function.py
@@ -11,7 +11,21 @@ my_canvas = Canvas(my_root, width = '300', height = '300')
 my_canvas.pack()
 
 def square_drawing (size, color):
-    box = my_canvas.create_rectangle((150 - (size / 2)), (150 - (size / 2)), (150 + (size / 2)), (150 + (size / 2)), fill = color)
+    rainbow = ['red', 'yellow', 'pink', 'green', 'purple', 'orange', 'blue']
+    basis = 150 - (size / 2)
+    unit = basis / 7
+    for i in range(6):
+        box = my_canvas.create_rectangle(0 + (i * unit), 0 + (i * unit), 300 - (i * unit), 300 - (i * unit), fill = rainbow[i])
 
-square_drawing(100, 'green')
+square_drawing(5, 'black')
 my_root.mainloop()
+
+
+
+    # box 300 distance - 0
+    # box 260 distance - 1 size
+    # box 220 distance - 2 size
+    # box 180 distance - 3 size
+    # box 140 distance - 4 size
+    # box 100 distance - 5 size
+    # box 60  size
