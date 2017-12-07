64c4be1a3d6eb1a1a332bb7f8c8e8a223090da8b
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-07 10:55:20 +0100

    Refining adder

--- week-03/day-04/02number_adder.py
+++ week-03/day-04/02number_adder.py
@@ -1,7 +1,7 @@
 # Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
 def adder(n):
-    if n < 0:
+    if n == 0:
         return 0
     else:
         return n + adder(n - 1)
