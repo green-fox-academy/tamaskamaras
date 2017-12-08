ba4e8266ff6474cc194c0dc2eabcff6c61d9302f
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-08 11:14:01 +0100

    16 break up inputs

--- week-03/day-03/16line_play_quarters.py
+++ week-03/day-03/16line_play_quarters.py
@@ -5,24 +5,23 @@ root = Tk()
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-def line_play(s, e):
-    for v in range(s, e, 20):
-        for h in range(s, e, 20):
-            if v == s:
-                sx = s
+def line_play(sv, sh, ev, eh):
+    for v in range(sv, ev, 20):
+        for h in range(sv, ev, 20):
+            if v == sv:
+                sx = v
                 sy = h
                 ex = h
-                ey = e - 20
+                ey = ev - 20
             line = canvas.create_line(sx, sy, ex, ey, fill = 'purple')
-            if h == s:
-                sx = e - 20
+            if h == sv:
+                sx = ev - 20
                 sy = v
                 ex = v
-                ey = s
+                ey = sv
             line = canvas.create_line(sx, sy, ex, ey, fill = 'green')
 
-line_play(0, 320)
-line_play(300, 620)
-# line_play(320, 320)
+line_play(0, 0, 320, 320)
+line_play(300, 300, 620, 620)
 
 root.mainloop()
