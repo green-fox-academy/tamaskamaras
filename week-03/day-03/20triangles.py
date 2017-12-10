808dadc6c5de76a2ac687efcb6e3cd363d18c1e6
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-10 12:00:01 +0100

    20 recursive triangle, multiple def calls

--- week-03/day-03/20triangles.py
+++ week-03/day-03/20triangles.py
@@ -8,17 +8,23 @@ size = 500
 canvas = Canvas(root, width = size, height = size)
 canvas.pack()
 
-# c = size // 2
-# d = size // 5
-
 def draw(c, d):
     triangle = canvas.create_polygon(c, d, c - 10, d + 20, c + 10, d + 20, fill = 'white', outline = 'black')
-    if c == 0 or d == 400:
+    if c == 0 or d == 300:
         return 0
     else:
         return c + draw(c - 10, d + 20)
 
-
 draw(250, 100)
+draw(260, 120)
+draw(270, 140)
+draw(280, 160)
+draw(290, 180)
+draw(300, 200)
+draw(310, 220)
+draw(320, 240)
+draw(330, 260)
+draw(340, 280)
+draw(350, 300)
 
 root.mainloop()
