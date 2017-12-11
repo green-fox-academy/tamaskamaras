7ff9ce43944d5578e5a9ec2bb9811d60b6c5df04
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-11 17:41:02 +0100

    04 Dice done

--- week-04/day-01/04_dice.py
+++ week-04/day-01/04_dice.py
@@ -17,18 +17,20 @@ class Dice(object):
             return self.dice
 
     def reroll(self, index = None):
-        for i in range(len(self.dice)):
-            if self.dice[i] == 6:
-                self.dice[i] == 6
-            else:
-                while self.dice[i] != 6:
-                    self.dice[i] = random.randint(1,6)
-                    if self.dice[i] == 6:
-                        self.dice[i] == 6
+        if index != None:
+            self.dice[index] = random.randint(1,6)
+        else:
+            self.roll()
 
 dice = Dice()
 print(dice.get_current())
 dice.roll()
 print(dice.get_current())
-dice.reroll()
+print(dice.get_current(3))
+print(dice.get_current())
+
+for i in range(len(dice.get_current())):
+    while dice.get_current(i) != 6:
+        dice.reroll(i)
+
 print(dice.get_current())
