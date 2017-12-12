876c3b31bd43bd7f52905359a00e0a7ae615e351
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-12 15:14:49 +0100

    01 Task done

--- week-04/day-02/01green_fox.py
+++ week-04/day-02/01green_fox.py
@@ -1,21 +1,19 @@
 class Person(object):
 
-    def __init__(self, name, age, gender):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female'):
         self.name = name
         self.age = age
         self.gender = gender
     
     def introduce(self):
-        print('Hi, I\'m {} a {} years old {}'.format(
-            self.name, self.age, self.gender
-            ))
+        print('Hi, I\'m {} a {} years old {}'.format(self.name, self.age, self.gender))
 
     def get_goal(self):
         print('My goal is: Live for the moment!')
     
 class Student(Person):
     
-    def __init__(self, name, age, gender, previous_organization, skipped_days):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', previous_organization = 'The School of Life', skipped_days = 0):
         super().__init__(name, age, gender)
         self.previous_organization = previous_organization
         self.skipped_days = skipped_days
@@ -26,9 +24,12 @@ class Student(Person):
     def introduce(self):
         print('Hi, I\'m {} a {} years old {} from {} who skipped {} days'.format(self.name, self.age, self.gender, self.previous_organization, self.skipped_days))
 
+    def skip_days(self, number_of_days):
+        self.skipped_days += number_of_days
+
 class Mentor(Person):
 
-    def __init__(self, name, age, gender, level):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', level = 'intermediate'):
         super().__init__(name, age, gender)
         self.level = level
     
@@ -40,7 +41,7 @@ class Mentor(Person):
 
 class Sponsor(Person):
 
-    def __init__(self, name, age, gender, company, hired_students):
+    def __init__(self, name = 'Jane Doe', age = 30, gender = 'female', company = 'Google', hired_students = 0):
         super().__init__(name, age, gender)
         self.company = company
         self.hired_students = hired_students
@@ -54,12 +55,12 @@ class Sponsor(Person):
     def get_goal(self):
         print('Hire brilliant junior software developers.')
 
-class PallidaClass():
+class PallidaClass(object):
 
-    def __init__(self, class_name):
+    def __init__(self, class_name, students = [], mentors = []):
         self.class_name = class_name
-        self.students = []
-        self.mentors = []
+        self.students = students
+        self.mentors = mentors
     
     def add_student(self, student):
         self.students.append(student)
@@ -70,24 +71,60 @@ class PallidaClass():
     def info(self):
         print('Pallida {} class has {} students and {} mentors'.format(self.class_name, len(self.students), len(self.mentors))) 
 
-person1 = Person('Jane Doe', 30, 'female')
-person1.introduce()
-person1.get_goal()
+# person1 = Person()
+# person1.introduce()
+# person1.get_goal()
 
-student1 = Student('Jane Doe', 30, 'female', 'The School of Life', 0)
-student1.introduce()
-student1.get_goal()
+# student1 = Student()
+# student1.introduce()
+# student1.get_goal()
 
-mentor1 = Mentor('Jane Doe', 30, 'female', 'intermediate')
-mentor1.introduce()
-mentor1.get_goal()
+# mentor1 = Mentor()
+# mentor1.introduce()
+# mentor1.get_goal()
 
-sponsor1 = Sponsor('Jane Doe', 30, 'female', 'Google', 0)
-sponsor1.hire()
-sponsor1.introduce()
-sponsor1.get_goal()
+# sponsor1 = Sponsor()
+# sponsor1.hire()
+# sponsor1.introduce()
+# sponsor1.get_goal()
 
-pallida1 = PallidaClass('Cool')
-pallida1.add_student(student1)
-pallida1.add_mentor(mentor1)
-pallida1.info()
+# pallida1 = PallidaClass('Cool')
+# pallida1.add_student(student1)
+# pallida1.add_mentor(mentor1)
+# pallida1.info()
+
+people = []
+
+mark = Person('Mark', 46, 'male')
+people.append(mark)
+jane = Person()
+people.append(jane)
+john = Student('John Doe', 20, 'male', 'BME')
+people.append(john)
+student = Student()
+people.append(student)
+gandhi = Mentor('Gandhi', 148, 'male', 'senior')
+people.append(gandhi)
+mentor = Mentor()
+people.append(mentor)
+sponsor = Sponsor()
+elon = Sponsor('Elon Musk', 46, 'male', 'SpaceX')
+people.append(elon)
+student.skip_days(3)
+
+for i in range(5):
+    elon.hire()
+
+for i in range(3):
+    sponsor.hire()
+
+for member in people:
+    member.introduce()
+    member.get_goal()
+
+badass = PallidaClass('BADA55')
+badass.add_student(student);
+badass.add_student(john);
+badass.add_mentor(mentor);
+badass.add_mentor(gandhi);
+badass.info();
