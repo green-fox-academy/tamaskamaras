78d716b41dc0fa7b1bfab6f837d57e4e78a37b5b
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-21 15:28:26 +0100

    feat: add string animals to txt line by line

--- week-05/favourite_animals.py
+++ week-05/favourite_animals.py
@@ -4,7 +4,9 @@ def fav_animals():
     if len(sys.argv) == 1:
         print('fav_animals [animal] [animal]')
     elif len(sys.argv) > 1:
-        with open('favourites.txt', 'a') as f:
-            f.write(sys.argv[-1] + '\n')
+        for item in (sys.argv[1:]):
+            # print(item)
+            with open('favourites.txt', 'a') as f:
+                f.write(item + '\n')
 
 fav_animals()
