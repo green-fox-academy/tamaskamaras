7dc4321de91462bef670ed7e3e14519988f39ccb
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-21 17:45:30 +0100

    Pallida exam Favourites works

--- week-05/favourite_animals.py
+++ week-05/favourite_animals.py
@@ -7,19 +7,12 @@ def fav_animals():
     elif len(sys.argv) > 1:
         temp = open('favourites.txt', 'r')
         anims = temp.readlines()
-        for item in (sys.argv[1:]):
-            pass
         for anim in anims:
             sum += anim
+        for item in (sys.argv[1:]):
             if item not in sum:
-
-                print(item)
-            # print(sum)
-            # print(anim)
-                    # sum += 1
-                    # if sum == 0:
-                    #     with open('favourites.txt', 'a') as f:
-                    #         f.write(item + '\n')
+                with open('favourites.txt', 'a') as f:
+                    f.write(item + '\n')
         temp.close()
 
 fav_animals()
