2f0b739abb0e329b0d2529a378479eb8462c014d
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-21 17:29:54 +0100

    feat: identify the differenc in input and stack

--- week-05/favourite_animals.py
+++ week-05/favourite_animals.py
@@ -1,12 +1,25 @@
 import sys
 
 def fav_animals():
+    sum = ''
     if len(sys.argv) == 1:
         print('fav_animals [animal] [animal]')
     elif len(sys.argv) > 1:
+        temp = open('favourites.txt', 'r')
+        anims = temp.readlines()
         for item in (sys.argv[1:]):
-            # print(item)
-            with open('favourites.txt', 'a') as f:
-                f.write(item + '\n')
+            pass
+        for anim in anims:
+            sum += anim
+            if item not in sum:
+
+                print(item)
+            # print(sum)
+            # print(anim)
+                    # sum += 1
+                    # if sum == 0:
+                    #     with open('favourites.txt', 'a') as f:
+                    #         f.write(item + '\n')
+        temp.close()
 
 fav_animals()
