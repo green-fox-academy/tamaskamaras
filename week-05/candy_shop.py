e5b1d31b3eb31f83de79c08d44bc50c3425b50cd
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-26 22:15:53 +0100

    feat: start to establish the architecture and create_sweet method

---
+++ week-05/candy_shop.py
@@ -0,0 +1,65 @@
+# We run a Candy shop where we sell candies and lollipops
+# One lollipop's price is 10$
+# And it made from 5gr of sugar
+# One candie's price is 20$
+# And it made from 10gr of sugar
+# we can raise their prices with a given percentage
+#
+# Create a CandyShop class
+# It can store sugar and money as income. The constructor should take the amount of sugar in gramms.
+# we can create lollipops and candies store them in the CandyShop's storage
+# If we create a candie or lollipop the CandyShop's sugar amount gets reduced by the amount needed to create the sweets
+# We can raise the prices of all candies and lollipops with a given percentage
+# We can sell candie or lollipop with a given number as amount
+# If we sell sweets the income will be increased by the price of the sweets and we delete it from the inventory
+# We can buy sugar with a given number as amount. The price of 1000gr sugar is 100$
+# If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income by the price of it.
+# The CandyShop should be represented as string in this format:
+# "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
+
+class CandyShop(object):
+    def __init__(self, sugar_gr):
+        self.sugar_gr = sugar_gr
+        self.income = 0
+        self.candies = 0
+        self.lollipops = 0
+    
+    def create_sweets(self, sweet):
+        if sweet == 'candy':
+            self.candies += 1
+        elif sweet == 'lollipop':
+            self.lollipops += 1
+        else:
+            print('This candy is not stored in our shop.')
+    
+    def raise_prices(self, percentage):
+        pass
+    
+    def sell(self, sweet, amount):
+        pass
+    
+    def buy_sugar(self, amount):
+        pass
+
+
+candy_shop = CandyShop(300)
+candy_shop.create_sweets("candy")
+candy_shop.create_sweets("candy")
+candy_shop.create_sweets("lollipop")
+candy_shop.create_sweets("lollipop")
+print(candy_shop)
+# Should print out:
+# Invetory: 2 candies, 2 lollipops, Income: 0, Sugar: 270gr
+candy_shop.sell("candy", 1)
+print(candy_shop)
+# Should print out:
+# "Invetory: 1 candies, 2 lollipops, Income:20, Sugar: 270gr"
+candy_shop.raise_prices(5)
+candy_shop.sell("lollipop", 1)
+print(candy_shop)
+# Should print out:
+# "Invetory: 1 candies, 1 lollipops, Income:30.5, Sugar: 270gr"
+candy_shop.buy_sugar(300)
+print(candy_shop)
+# Should print out:
+# "Invetory: 1 candies, 1 lollipops, Income:5, Sugar: 570gr"
