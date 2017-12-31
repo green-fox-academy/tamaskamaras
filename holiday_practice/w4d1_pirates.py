ed38f3d895f271eb34ad396cc7e52343b7622377
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-31 13:19:54 +0100

    feat: all pirates method has been tested

--- holiday_practice/w4d1_pirates.py
+++ holiday_practice/w4d1_pirates.py
@@ -4,34 +4,36 @@ class Pirate(object):
     def __init__(self):
         self.state = 'Awake'
         self.drinks = 0
+        self.parrot = 'Parrot'
     
     def drink_sum_rum(self):
         if self.state == 'Dead':
-            return print('He\'s dead!')
+            return 'He\'s dead!'
         else:
             self.drinks += 1
+            if self.drinks > 4:
+                self.state = 'Passed out'
     
     def hows_it_going_mate(self):
         if self.state == 'Dead':
-            return print('He\'s dead!')
+            return 'He\'s dead!'
         elif self.drinks <= 4:
-            return print('Pour me anudder!')
+            return 'Pour me anudder!'
         else:
-            self.state = 'Passed out'
-            return print('Arghh, I\'ma Pirate. How d\'ya d\'ink its goin?')
+            return 'Arghh, I\'ma Pirate. How d\'ya d\'ink its goin?'
     
     def die(self):
         self.state = 'Dead'
     
     def brawl(self, enemy):
         if enemy.state != 'Dead':
-            if randint(1, 3) == 1:
+            rand = random.randint(1, 3)
+            if rand == 1:
                 self.state = 'Dead'
-            elif randint(1, 3) == 2:
+            elif rand == 2:
                 enemy.state = 'Dead'
             else:
                 self.state = 'Dead'
                 enemy.state = 'Dead'
-
-pirate1 = Pirate()
-pirate2 = Pirate()
+        else:
+            return 'Your enemy is already dead!'
--- holiday_practice/w4d1_pirates_test.py
+++ holiday_practice/w4d1_pirates_test.py
@@ -1,15 +1,58 @@
 import unittest
 from w4d1_pirates import Pirate
 
-
 class TestPirates(unittest.TestCase):
 
     def setUp(self):
         self.object = Pirate()
+        self.enemy_pirate = Pirate()
 
     def test_drinks(self):
         self.object.drink_sum_rum()
         self.assertEqual(self.object.drinks, 1)
+
+    def test_5_drinks(self):
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.assertEqual(self.object.drinks, 5)
+    
+    def test_ask(self):
+        self.assertEqual(self.object.hows_it_going_mate(), 'Pour me anudder!')
+
+    def test_passed_out(self):
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.assertEqual(self.object.state, 'Passed out')
+
+    def test_hows_going(self):
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.object.drink_sum_rum()
+        self.assertEqual(self.object.hows_it_going_mate(), 'Arghh, I\'ma Pirate. How d\'ya d\'ink its goin?')
+    
+    def test_dead(self):
+        self.object.state = 'Dead'
+        self.assertEqual(self.object.drink_sum_rum(), 'He\'s dead!')
+    
+    def test_die(self):
+        self.object.die()
+        self.assertEqual(self.object.state, 'Dead')
+    
+    def test_brawl_self(self):
+        self.object.brawl(self.enemy_pirate)
+        self.assertEqual(self.object.state, 'Awake')
+
+    def test_brawl_enemy(self):
+        self.object.brawl(self.enemy_pirate)
+        self.assertEqual(self.enemy_pirate.state, 'Dead')
 
 if __name__ == '__main__':
     unittest.main()
