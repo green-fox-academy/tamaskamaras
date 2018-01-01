e631444ed31ba608b954e291d486d5da5d450136
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-01 16:12:56 +0100

    feat: create w4d2 carrier test

---
+++ holiday_practice/w4d2_carrier_test.py
@@ -0,0 +1,19 @@
+import unittest
+from w4d2_carrier import *
+
+class TestCarrier(unittest.TestCase):
+    def setUp(self):
+        self.f16 = F16()
+    
+    def test_damage(self):
+        self.assertEqual(self.f16.fight(), 0)
+    
+    def test_refill(self):
+        self.assertEqual(self.f16.refill(300), 292)
+    
+    def test_refill_ammo(self):
+        self.f16.refill(300)
+        self.assertEqual(self.f16.ammo, 8)
+
+if __name__ == '__main__':
+    unittest.main()
