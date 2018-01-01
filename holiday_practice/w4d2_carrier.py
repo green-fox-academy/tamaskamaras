e2601d34ff072f70617832779602ceda9f648a87
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-01 16:50:23 +0100

    feat: create w4d2 carrier __str__ method

--- holiday_practice/w4d2_carrier.py
+++ holiday_practice/w4d2_carrier.py
@@ -1,12 +1,15 @@
 class Aircraft(object):
-    def __init__(self, ammo, max_ammo, base_damage):
+    def __init__(self, ammo, max_ammo, base_damage, type, all_damage):
         self.ammo = ammo
         self.max_ammo = max_ammo
         self.base_damage = base_damage
+        self.type = type
+        self.all_damage = all_damage
 
     def fight(self):
         damage = self.ammo * self.base_damage
         self.ammo = 0
+        self.all_damage += damage
         return damage
     
     def refill(self, amount):
@@ -14,17 +17,16 @@ class Aircraft(object):
         self.ammo += amount - remaining
         return remaining
 
+    def get_type(self):
+        return self.type
+    
+    def __str__(self):
+        return 'Type ' + self.type + ', Ammo: ' + str(self.ammo) + ', Base damage: ' + str(self.base_damage) + ', All damage: ' + str(self.all_damage)
+
 class F16(Aircraft):
     def __init__(self):
-        super().__init__(0, 8, 30)
-
-    def get_type(self):
-        return 'F16'
+        super().__init__(0, 8, 30, 'F16', 0)
 
 class F35(Aircraft):
     def __init__(self):
-        super().__init__(0, 12, 50)
-
-    def get_type(self):
-        return 'F35'
-    
+        super().__init__(0, 12, 50, 'F35', 0)
