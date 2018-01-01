6b5020de5d600201f5975737fef40c7c70961d18
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-01 18:36:45 +0100

    feat: test w4d2 carrier add_aircraft method

--- holiday_practice/w4d2_carrier_test.py
+++ holiday_practice/w4d2_carrier_test.py
@@ -4,6 +4,7 @@ from w4d2_carrier import *
 class TestCarrier(unittest.TestCase):
     def setUp(self):
         self.f16 = F16()
+        self.carrier = Carrier(1000, 100)
     
     def test_damage(self):
         self.assertEqual(self.f16.fight(), 0)
@@ -25,6 +26,10 @@ class TestCarrier(unittest.TestCase):
         self.f16.refill(8)
         self.f16.fight()
         self.assertEqual(self.f16.__str__(), 'Type F16, Ammo: 0, Base damage: 30, All damage: 240')
+    
+    def test_add_aircraft(self):
+        self.carrier.add_aircraft('F35')
+        self.assertEqual(len(self.carrier.aircrafts), 1)
 
 if __name__ == '__main__':
     unittest.main()
