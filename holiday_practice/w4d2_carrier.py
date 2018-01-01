36fa11463432fb64a30848d75f63a154eb072377
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-01 15:59:14 +0100

    feat: create w4d2 carrier Aircraft class and subclasses

---
+++ holiday_practice/w4d2_carrier.py
@@ -0,0 +1,18 @@
+class Aircraft(object):
+    def __init__(self, ammo, max_ammo, base_damage):
+        self.ammo = ammo
+        self.max_ammo = max_ammo
+        self.base_damage = base_damage
+
+    def fight(self):
+        damage = self.ammo * self.base_damage
+        self.ammo = 0
+        return damage
+
+class F16(Aircraft):
+    def __init__(self):
+        super().__init__(0, 8, 30)
+
+class F35(Aircraft):
+    def __init__(self):
+        super().__init__(0, 12, 50)
