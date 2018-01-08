c7535282499a0eb6bf9d511adfd5f4c9d3b9b3a9
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-08 12:40:21 +0100

    feat: task 10

---
+++ JS/week-07/day-1/a10define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var name = 'Tamas Kamaras ';
+var age = 33;
+var height = 1.85;
+var married = false;
+
+console.log(name + age + height + married);
