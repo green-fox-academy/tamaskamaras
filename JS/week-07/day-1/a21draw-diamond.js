44a38dc0ad04942756a02760f8a0f040be0e1f84
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-08 21:28:44 +0100

    feat: create perfectly responsive diamond

--- JS/week-07/day-1/a21draw-diamond.js
+++ JS/week-07/day-1/a21draw-diamond.js
@@ -14,10 +14,10 @@
 //
 // The diamond should have as many lines as lineCount is
 
-var lineCount = 8;
+var lineCount = 7;
 
+console.log('')
 let upIteration = Math.ceil(lineCount / 2);
-
 let stars = '*'
 for (let i = 1; i < upIteration + 1; i++) {
     console.log(
@@ -28,46 +28,14 @@ for (let i = 1; i < upIteration + 1; i++) {
 }
 
 let downIteration = Math.floor(lineCount / 2);
-
 let varia = 0
-
 if (lineCount % 2 !== 0) {
     varia += 1;
 }
 
 for (let j = 0; j < downIteration; j++) {
     console.log(
-        'x'.repeat(j + varia)
+        ' '.repeat(j + varia) +
+        '*'.repeat((downIteration + 3 - varia) - (j * 2) )
     );
 }
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////
-
-// console.log(' ')
-// let star1 = '*';
-// let spaces1 = (lineCount / 2);
-
-// for (let i = 1; i < ((lineCount / 2) + 1); i++) {
-//     console.log(
-//         ' '.repeat(spaces1) + 
-//         star1);
-//     star1 += '**';
-//     spaces1--;
-// }
-
-// let iteration = Math.floor(lineCount / 2)
-
-// let star2 = iteration + 2
-// for (let a = 1; a < (iteration + 1); a++) {
-//     console.log(' '.repeat(a) + '*'.repeat(star2))
-//     star2 -= 2;
-// }
