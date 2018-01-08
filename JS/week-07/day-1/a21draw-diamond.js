a86f7bee72d41a64c43f58497f6c717525267659
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-08 18:44:53 +0100

    feat: refactor diamond

--- JS/week-07/day-1/a21draw-diamond.js
+++ JS/week-07/day-1/a21draw-diamond.js
@@ -16,22 +16,55 @@
 
 var lineCount = 7;
 
-console.log(' ')
-let star1 = '*';
-let spaces1 = (lineCount / 2);
+let upIteration = Math.ceil(lineCount / 2);
 
-for (let i = 1; i < ((lineCount / 2) + 1); i++) {
+let stars = '*'
+for (let i = 1; i < upIteration + 1; i++) {
     console.log(
-        ' '.repeat(spaces1) + 
-        star1);
-    star1 += '**';
-    spaces1--;
+        ' '.repeat(upIteration - i) +
+        stars
+    )
+    stars += '**'
 }
 
-let iteration = Math.floor(lineCount / 2)
-
-let star2 = iteration + 2
-for (let a = 1; a < (iteration + 1); a++) {
-    console.log(' '.repeat(a) + '*'.repeat(star2))
-    star2 -= 2;
+let downIteration = Math.floor(lineCount / 2);
+for (let i = 0; i < upIteration + 1; i++) {
+    console.log(
+        ' '.repeat(i) +
+        '*'.repeat(2 * upIteration - (i * 2) ))
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////
+
+// console.log(' ')
+// let star1 = '*';
+// let spaces1 = (lineCount / 2);
+
+// for (let i = 1; i < ((lineCount / 2) + 1); i++) {
+//     console.log(
+//         ' '.repeat(spaces1) + 
+//         star1);
+//     star1 += '**';
+//     spaces1--;
+// }
+
+// let iteration = Math.floor(lineCount / 2)
+
+// let star2 = iteration + 2
+// for (let a = 1; a < (iteration + 1); a++) {
+//     console.log(' '.repeat(a) + '*'.repeat(star2))
+//     star2 -= 2;
+// }
