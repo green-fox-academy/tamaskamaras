7719e846b7193e73b3fba78c96d7724d2da448c0
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-08 12:13:35 +0100

    feat: task 4

---
+++ JS/week-07/day-1/a04introduce-yourself.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+
+console.log('Tamas Kamaras')
+console.log(33)
+console.log(1.85)
