084a20e0d3765aef93f0b38cb2fa01b7c00953cd
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-09 12:54:05 +0100

    feat: create greeter

---
+++ JS/week-07/day-2/func02_greet.js
@@ -0,0 +1,20 @@
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+'use strict'
+
+let al = 'Greenfox';
+
+function greet(someone) {
+    if (someone === undefined) {
+        console.log('Greetings, all,')
+    } else {
+        console.log('Greetings, dear ' + someone)
+    }
+}
+
+greet();
+greet(al);
