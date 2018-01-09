a5d62fda3732fdb0118c0b70b285d36af797b08e
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-09 17:26:12 +0100

    feat: create student filter

---
+++ JS/week-07/day-2/data/object2_student_filter.js
@@ -0,0 +1,25 @@
+// create a function that takes a list of students and logs:
+// - Who has got more candies than 4 candies
+
+// create a function that takes a list of students and logs: 
+//  - how many candies they have on average
+
+'use strict';
+
+var students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+
+function getCandies(list) {
+    let owners = []
+    students.forEach(function(element) {
+        if (element['candies'] > 4)
+        owners.push(element['name'])
+    })
+    return owners
+}
+
+console.log(getCandies(students));
