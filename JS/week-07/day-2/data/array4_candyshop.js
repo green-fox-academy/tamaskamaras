53fe7a97cc0d5468a418ee45e346a3d86f93ad9d
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-10 11:34:03 +0100

    feat: create candyshop

---
+++ JS/week-07/day-2/data/array4_candyshop.js
@@ -0,0 +1,12 @@
+// Accidentally we added "2" and "false" to the array. 
+// Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+// No, don't just remove the items :)
+
+'use strict';
+
+var shop_items = ["Cupcake", 2, "Brownie", false]
+
+shop_items[1] = 'Croissant';
+shop_items[3] = 'Ice cream';
+
+console.log(shop_items);
