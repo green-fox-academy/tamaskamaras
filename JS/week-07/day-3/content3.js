3d872842512281d11f055641c61dddabfbb6b4fb
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-10 17:02:24 +0100

    feat: create content3

---
+++ JS/week-07/day-3/content3.js
@@ -0,0 +1,5 @@
+'use strict'
+
+document.querySelector('.output1').textContent = document.querySelector('p').textContent;
+
+document.querySelector('.output2').innerHTML = document.querySelector('p').innerHTML;
