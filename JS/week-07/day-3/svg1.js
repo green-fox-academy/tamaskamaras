1a896a3a27d1b7da6bd92c61991a64e727ba0c35
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-11 17:13:50 +0100

    feat: create svg 1

---
+++ JS/week-07/day-3/svg1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let rectangle = document.querySelector('#very_rectangle');
+
+rectangle.setAttribute('x', 50);
+rectangle.setAttribute('y', 50);
+rectangle.setAttribute('style', 'stroke:#ff0000; stroke-width: 10; fill: tomato');
