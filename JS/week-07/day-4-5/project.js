504887394cb6744557278ef1ffc4b464ee1adffe
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-12 15:58:22 +0100

    feat: create the right steping method

--- JS/week-07/day-4-5/project.js
+++ JS/week-07/day-4-5/project.js
@@ -3,20 +3,34 @@
 let centerImage = document.querySelector('img');
 let galeryItems = document.querySelector('#thumbnail');
 
+let imageUrls = [];
+document.querySelectorAll('#thumbnail > a > img').forEach(function(element, i) {
+    imageUrls.push(element.src)
+});
+
 let leftButton = document.querySelector('.side:first-child')
 let rightButton = document.querySelector('.side:last-child')
 
 galeryItems.addEventListener('click', changePicture);
-// leftButton.addEventListener('click', )
-// rightButton.addEventListener('click', )
+// leftButton.addEventListener('click', );
+rightButton.addEventListener('click', stepPicture);
 
 var current = 'file:///C:/Users/K/Desktop/greenfox/tamaskamaras/JS/week-07/day-4-5/image01.jpg';
 
-console.log(current);
+var clicks = 1;
+
+function stepPicture() {
+    centerImage.setAttribute('src', imageUrls[clicks])
+    clicks += 1
+};
 
 function changePicture(e) {
     centerImage.setAttribute('src', e.target.src)
     document.querySelector('h1').textContent = e.target.alt
     current = e.target.src
-    console.log(current);
+    imageUrls.forEach(function(element, i) {
+        if (element === e.target.src) {
+            clicks = i + 1
+        }
+    })
 };
