027ecc5b2a241b887fc3b54a355749dc83cf9e08
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-12 12:19:45 +0100

    feat: display the image names in the center

--- JS/week-07/day-4-5/project.js
+++ JS/week-07/day-4-5/project.js
@@ -4,6 +4,10 @@ document.querySelector('#thumbnail').addEventListener('click', changePicture);
 
 let centerImage = document.querySelector('img');
 
+
+
 function changePicture(e) {
     centerImage.setAttribute('src', e.target.src)
+    document.querySelector('h1').textContent = e.target.alt
 };
+
