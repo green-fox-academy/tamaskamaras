33b245069513ef7fee898464d0d4ce459d7acc54
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-12 12:58:33 +0100

    feat: get the current element in the center

--- JS/week-07/day-4-5/project.js
+++ JS/week-07/day-4-5/project.js
@@ -1,13 +1,22 @@
 'use strict'
 
-document.querySelector('#thumbnail').addEventListener('click', changePicture);
-
 let centerImage = document.querySelector('img');
+let galeryItems = document.querySelector('#thumbnail');
 
+let leftButton = document.querySelector('.side:first-child')
+let rightButton = document.querySelector('.side:last-child')
 
+galeryItems.addEventListener('click', changePicture);
+// leftButton.addEventListener('click', )
+// rightButton.addEventListener('click', )
+
+var current = 'file:///C:/Users/K/Desktop/greenfox/tamaskamaras/JS/week-07/day-4-5/image01.jpg';
+
+console.log(current);
 
 function changePicture(e) {
     centerImage.setAttribute('src', e.target.src)
     document.querySelector('h1').textContent = e.target.alt
+    current = e.target.src
+    console.log(current);
 };
-
