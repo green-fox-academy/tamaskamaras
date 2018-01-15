1f50745c9d9c24fa94c512f5593341e1162cb731
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-15 17:53:15 +0100

    feat: create classes and constructors in elevator js

--- JS/week-08/day-1/elevator.js
+++ JS/week-08/day-1/elevator.js
@@ -1,1 +1,30 @@
 'use strict';
+
+class ElevatorController {
+
+    constructor(maxFloor, maxPeople) {
+        this.maxFloor = maxFloor;
+        this.maxPeople = maxPeople;
+        let model = new ElevatorModel(this.maxFloor, this.maxPeople);
+        let view = new ElevatorView(model.maxFloor);
+    };
+};
+
+class ElevatorModel {
+
+    constructor(maxFloor, maxPeople) {
+        this.maxFloor = maxFloor;
+        this.maxPeople = maxPeople;
+    };   
+};
+
+class ElevatorView{
+
+    constructor(floors) {
+        this.floors = floors;
+    };
+
+    drawElevator()
+};
+
+let myElevator = new ElevatorController(10, 10);
