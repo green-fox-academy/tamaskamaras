98f38cc6d2c1726d5a46331f8fd349ba4b717c28
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-16 00:17:11 +0100

    feat: creating the moveing methods

--- JS/week-08/day-1/elevator.js
+++ JS/week-08/day-1/elevator.js
@@ -1,12 +1,18 @@
 'use strict';
 
 class ElevatorController {
-
+    
     constructor(maxFloor, maxPeople) {
         this.maxFloor = maxFloor;
         this.maxPeople = maxPeople;
+        this.buttonUp = document.getElementById('up');
+        this.buttonDown = document.getElementById('down');
         let model = new ElevatorModel(this.maxFloor, this.maxPeople);
         let view = new ElevatorView(model.maxFloor);
+        let moveUp = model.moveUp.bind(model);
+        let moveDown = model.moveDown.bind(model);
+        this.buttonUp.addEventListener('click', moveUp);
+        this.buttonDown.addEventListener('click', moveDown);
     };
 };
 
@@ -15,16 +21,46 @@ class ElevatorModel {
     constructor(maxFloor, maxPeople) {
         this.maxFloor = maxFloor;
         this.maxPeople = maxPeople;
-    };   
+        this.ground = 1;
+        console.log(this.ground);
+    };
+
+    moveUp() {
+        console.log(this.ground);
+        if (this.ground < this.maxFloor) {
+            document.getElementById('f' + this.ground).className = 'elevator';
+            document.getElementById('f' + (this.ground - 1)).classList.remove('elevator');
+            this.ground++;
+        }
+    };
+
+    moveDown() {
+        console.log(this.ground);
+        if (this.ground > 0) {
+            document.getElementById('f' + (this.ground - 1)).className = 'elevator';
+            document.getElementById('f' + (this.ground)).classList.remove('elevator');
+            this.ground--;
+        }
+    };
 };
 
 class ElevatorView{
 
     constructor(floors) {
         this.floors = floors;
+        this.drawElevator();
     };
-
-    drawElevator()
+    
+    drawElevator() {
+        let shaft = document.querySelector('div');
+        for (let i = 0; i < this.floors + 1; i++) {
+            let floor = document.createElement('p');
+            floor.id = 'f' + i;
+            floor.textContent = i;
+            shaft.appendChild(floor);
+        }
+        document.getElementById('f0').className = 'elevator';
+    }
 };
 
 let myElevator = new ElevatorController(10, 10);
