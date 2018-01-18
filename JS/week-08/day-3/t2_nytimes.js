5e688c44d5aebd11535f435f7764a3b32bf12e89
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-18 20:26:32 +0100

    fix: refactor and styleguide nytimes

--- JS/week-08/day-3/t2_nytimes.js
+++ JS/week-08/day-3/t2_nytimes.js
@@ -1,26 +1,22 @@
 'use strict';
 
-// Article Search API: 50d785df66334c9780725773581c3bca
-
 let myRequest = new XMLHttpRequest();
+let apiKey = '50d785df66334c9780725773581c3bca';
 myRequest.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Apollo+11+moon+landing&api_key=50d785df66334c9780725773581c3bca');
-myRequest.send();
 myRequest.onreadystatechange = function() {
-    let article = [];
-    if (myRequest.readyState === 4) {
-        console.log(JSON.parse(myRequest.response).response.docs[0]);
-        // document.write(JSON.parse(myRequest.response).response.docs[0].web_url);
-        let link = (JSON.parse(myRequest.response).response.docs[0].web_url);
-        let temp = JSON.parse(myRequest.response).response.docs[0];
-        article.push(temp.headline.main);
-        // console.log('Article logging:', article);
-        permalink(link);
-    };
+	let article = [];
+	if (myRequest.readyState === 4) {
+		let link = (JSON.parse(myRequest.response).response.docs[0].web_url);
+		let temp = JSON.parse(myRequest.response).response.docs[0];
+		article.push(temp.headline.main);
+		permalink(link);
+	};
 };
+myRequest.send();
 
 function permalink(link) {
-    let permalink = document.createElement('a');
-    permalink.setAttribute('href', link);
-    permalink.textContent = link;
-    document.querySelector('body').appendChild(permalink);
-}
+	let permalink = document.createElement('a');
+	permalink.setAttribute('href', link);
+	permalink.textContent = link;
+	document.querySelector('body').appendChild(permalink);
+};
