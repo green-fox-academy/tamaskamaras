4abb44b30e0eac0b02e715943f5de8348554495b
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-25 17:35:17 +0100

    feat: create append a task

--- JS/week-09/day-4/frontend_back.js
+++ JS/week-09/day-4/frontend_back.js
@@ -3,6 +3,7 @@
 const express = require('express');
 
 const app = express();
+let post = 8080;
 
 app.use('/assets', express.static('assets'))
 
@@ -10,4 +11,41 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
 });
 
-app.listen(8080);
+app.get('/doubling', function(req, res) {
+  let temp = {}
+  if (req.query.input === undefined) {
+    temp = {"error": "Please provide an input!"}
+  } else {
+    temp = {
+          "received": req.query.input,
+          "result": (2 * req.query.input)
+        }
+  }
+  res.send(temp);
+})
+
+app.get('/greeter', function(req, res) {
+  let greet = {};
+  if (req.query.name === undefined) {
+    greet = {"error": "Please provide a name!"};
+  } else if (req.query.title === undefined) {
+    greet = {"error": "Please provide a title!"};
+  } else {
+    greet = {"welcome_message": "Oh, hi there " + req.query.name + ", my dear " + req.query.title + "!"};
+  }
+  res.send(greet);
+});
+
+app.get('/appenda/:word', function(req, res) {
+  let result = {};
+  if (req.params.word.length > 0) {
+    result = {
+      "appended": (req.params.word + 'a')
+    }
+    res.send(result)
+  }
+});
+
+app.listen(8080, function() {
+  console.log(`App is listening on ${post}`)
+});
