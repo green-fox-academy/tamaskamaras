ee16a79f82180e4a92ac9a3583c545c11ec23950
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2018-01-25 14:01:30 +0100

    feat: rendering html file + serve static file

---
+++ JS/week-09/day-4/frontend_back.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use('/assets', express.static('assets'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(8080);
