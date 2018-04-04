c22b3dc9c1809dbffcae5414c967ba1c23ce07f6
Tamas Kamaras <kamaras.tamas.viktor@gmail.com>
2018-04-04 21:05:28 +0200

    fix: dependency injection implemented

--- partner_tests/state_machine/v02/index_refactor.js
+++ partner_tests/state_machine/v02/index_refactor.js
@@ -6,17 +6,75 @@ function Child(name) {
 	this.mood = 0;
 	this.snacks = 0;
   this.snackConsumption = 0;
-  this.callToKindergarten = () => {};
-  this.callToPlay = () => {};
-  this.callToEat = () => {};
+  this.callToKindergarten = (state) => {
+    let from = state || this.state;
+    this.state = 'hiding';
+		return `${this.name}'s state transited from "${from}" to "${this.state}".`;
+  };
+  this.callToPlay = (state) => {
+    let from = state || this.state;
+    consumeSnack(this);
+    if (from !== 'playing') {
+      this.state = 'playing';
+      return `${this.name}'s state transited from "${from}" to "${this.state}".`;    
+    }
+		return `Cannot transit from "${from}" with "call to play".`;
+  };
+  this.callToEat = (state) => {
+    let from = state || this.state;
+    if (from === 'playing') {
+      this.state = 'eating';
+			return `${this.name}'s state transited from "${from}" to "${this.state}".`;
+    }
+		return `Cannot transit from "${from}" with "call to eat".`;
+  };
 }
 
+function giveSnack (state, child) {
+  let toChild = child || mock;
+  let position = state || toChild.state;
+  (!child) ? toChild.snacks = 0 : null;
+  if (position === 'playing' || position === 'eating') {
+    toChild.snacks++;
+  }
+  return toChild.snacks;
+}
 
+function consumeSnack (state, child) {
+  let toChild = child || mock;
+  let position = state || toChild.state;
+  (!child) ? toChild.snacks = 1 : null;
+  if (position === 'eating') {
+    toChild.snacks = 0;
+  }
+  return toChild.snacks;
+}
+
+function raiseMood (child) {
+}
+
+let mock = new Child('Mock');
+let pete = new Child('Pete');
 
 function displayChild(child) {}
 
+// console.log(pete.state);
+// giveSnack(pete);
+// giveSnack(pete);
+// giveSnack(pete);
+// giveSnack(pete);
+// console.log(pete.snacks);
 
 
+// console.log(giveSnack());
+// console.log(pete.callToPlay('hiding'));
+// console.log(pete.state);
+// console.log(pete.callToEat());
+// console.log(pete.state);
 
-let pete = new Child('Pete');
-
+module.exports = {
+  Child,
+  giveSnack,
+  consumeSnack,
+  raiseMood
+}
--- partner_tests/state_machine/v02/test.js
+++ partner_tests/state_machine/v02/test.js
@@ -1,30 +1,109 @@
 'use strict';
 
 const test = require('tape');
-const app = require('./index');
+const app = require('./index_refactor');
 
 let pete = new app.Child('Pete');
 
-test('give snack', (t) => {
-  let actual = app.stateMachine.callToKindergarten(pete);
-  let expected = `Pete's state transited from "playing" to "hiding"`;
+test('call to kindergarten from playing', (t) => {
+  let actual = pete.callToKindergarten();
+  let expected = `Pete's state transited from "playing" to "hiding".`;
   
   t.equal(actual, expected);
   t.end();
 });
 
-test('call to eat / fail', (t) => {
-  let actual = app.stateMachine.callToEat(pete);
-  let expected = `Cannot transition from "hiding" with "call to eat"`;
+test('call to kindergarten from eating', (t) => {
+  let actual = pete.callToKindergarten('eating');
+  let expected = `Pete's state transited from "eating" to "hiding".`;
+  
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to kindergarten from hiding', (t) => {
+  let actual = pete.callToKindergarten('hiding');
+  let expected = `Pete's state transited from "hiding" to "hiding".`;
+  
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to play from playing', (t) => {
+  let actual = pete.callToPlay('playing');
+  let expected = `Cannot transit from "playing" with "call to play".`;
 
   t.equal(actual, expected);
   t.end();
 });
 
-test('call to eat', (t) => {
-  pete.state = 'playing';
-  let actual = app.stateMachine.callToEat(pete);
-  let expected = `Pete\'s state transited from "playing" to "eating"`;
+test('call to play from eating', (t) => {
+  let actual = pete.callToPlay('eating');
+  let expected = `Pete's state transited from "eating" to "playing".`;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to play from hiding', (t) => {
+  let actual = pete.callToPlay('hiding');
+  let expected = `Pete's state transited from "hiding" to "playing".`;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to eat from playing', (t) => {
+  let actual = pete.callToEat('playing');
+  let expected = `Pete's state transited from "playing" to "eating".`;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to eat from eating', (t) => {
+  let actual = pete.callToEat('eating');
+  let expected = `Cannot transit from "eating" with "call to eat".`;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('call to eat from hiding', (t) => {
+  let actual = pete.callToEat('hiding');
+  let expected = `Cannot transit from "hiding" with "call to eat".`;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('give snack', (t) => {
+  let actual = app.giveSnack();
+  let expected = 1;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('give snack in hiding', (t) => {
+  let actual = app.giveSnack('hiding');
+  let expected = 0;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('consume snack in eating', (t) => {
+  let actual = app.consumeSnack('eating');
+  let expected = 0;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('consume snack in hiding', (t) => {
+  let actual = app.consumeSnack('hiding');
+  let expected = 1;
 
   t.equal(actual, expected);
   t.end();
